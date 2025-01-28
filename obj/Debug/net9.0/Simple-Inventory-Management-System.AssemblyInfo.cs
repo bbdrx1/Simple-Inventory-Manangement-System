3461bee80f2ea6dde78a9d56a376a7ca710dd352
bbdrx1 <labuanananmark21@gmail.com>
2025-01-28 12:19:07 +0800

    feat(be): develop simple inventory management system
    1. CRUD of product
    -AddProduct(Product product): Adds a product to the inventory.
    -RemoveProduct(int productId): Removes a product from the inventory based on its ID.
    -UpdateProduct(int productId, int newQuantity): Updates the quantity of a product.
    -ListProducts(): Displays all items in the inventory.
    -GetTotalValue(): Calculates and returns the total value of the inventory.
    
    2. Constraint
    -Product ID: Must be a positive integer and unique for each product. The system prompts for a valid ID if an invalid one is entered.
    -Quantity: Must be a non-negative integer (0 or greater). Negative quantities are not allowed.
    -Price: Must be a non-negative decimal number. Invalid price inputs are rejected.
    -Inventory Capacity: No specific limit on the number of products, but performance may degrade with a large inventory size.
    -Product Uniqueness: Each product must have a unique ID; the system does not handle duplicate IDs.
    -Removal and Update: Products that don’t exist in the inventory cannot be removed or updated, and an error message will be displayed if a non-existent                product ID is entered.

--- obj/Debug/net9.0/Simple-Inventory-Management-System.AssemblyInfo.cs
+++ obj/Debug/net9.0/Simple-Inventory-Management-System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simple-Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39aa4c8348e2030ff4a259372fe1b29b55aad44f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simple-Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simple-Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
